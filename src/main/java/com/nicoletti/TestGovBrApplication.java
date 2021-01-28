d4cbb88f1d3e60271da723bafb1bcf6b94e31656
Marcos Paulo Nicoletti <marcos.nicoletti@automatos.com>
2021-01-27 22:27:13 -0300

    Init project

---
+++ src/main/java/com/nicoletti/TestGovBrApplication.java
@@ -0,0 +1,13 @@
+package com.nicoletti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGovBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGovBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicoletti/TestGovBrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoletti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGovBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
